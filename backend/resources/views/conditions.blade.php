c63e09e06d9cb737db0184cca8b3de4fcf64de63
smainyounes <younes.abd.smain@gmail.com>
2021-09-19 03:07:01 +0200

    adding the conditions page

---
+++ backend/resources/views/conditions.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.layout')
+
+@section('title', 'Conditions générales d\'utilisation')
+
+@section('content')
+    <div class="container-fluid px-md-5 py-5" style="background: url({{ asset('img/banner/open_book.jpg') }}) no-repeat center center; margin-top: 66px; background-size: cover;">
+        <div class="">
+            <h3 class="text-white bold my-4">Conditions générales d'utilisation</h3>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div>
+            <h4 class="font-weight-600 mb-3">Conditions générales</h4>
+            <p class="font-weight-500">les modalités de mise à disposition des services du site www.tendaxe.com et les utilisations du site sont définis par les Conditions Générales d’utilisation ; toute utilisation du notre site  suppose l'acceptation et le respect de l'ensemble des Termes des présentes Conditions et leur acceptation inconditionnelle. Elles constituent donc un contrat entre le site et l'Utilisateur (moral ou physique) qui respecte les points suivants :</p>
+            <ul class="font-weight-500">
+                <li>Le site est accessible uniquement à tout utilisateur disposant d'un abonnement.</li>
+                <li>L’identifiant et le mot de passe restent personnels et confidentiels, Il appartient à l’utilisateur de prendre toutes les dispositions nécessaires permettant de protéger ses propres données contre toute atteinte.</li>
+                <li>Ne pas revendre un abonnement à une tierce personne.</li>
+                <li>Les sections du site sont réservées aux abonnés après identification à l'aide de leur identifiant et de leur mot de passe.</li>
+            </ul>
+        </div>
+
+        <hr>
+
+        <div>
+            <h4 class="bold font-weight-600 mb-3">Confidentialité / Sécurité</h4>
+            <p class="font-weight-500">TendAxe® assure à ses utilisateurs que leur navigation sur les différentes pages du site ne sont aucunement tracké ni enregistré sur la base de donnée.
+                TendAxe® assure à ses utilisateurs que toutes leurs informations personnelles sont enregistrés d’une manière crypté, nos accessible, ni lisible à toute personne tièrce du service.
+                TendAxe® assure qu’aucun logiciel / programme de tracking visé n’est installé sur son site web afin de garantir une confidentialité complète pour ses utilisateurs.</p>
+        </div>
+
+        <hr>
+
+        <div>
+            <h4 class="bold font-weight-600 mb-3">Accès au site</h4>
+            <p class="font-weight-500">
+                TendAxe® est accessible à tout utilisateur qui s’est préalablement acquitté des droits d’abonnement annuel.
+                TendAxe® se réserve la possibilité d'interrompre, de suspendre momentanément ou de nodifier avec préavis l'accès à tout ou partie du site, afin d'en assurer la maintenance.
+                TendAxe® ne peut, être tenue responsable de tout dysfonctionnement du réseau internet ou de tout autre événement échappant au contrôle raisonnable, qui empêcherait ou dégraderait l'accès au site.
+                TendAxe® met en œuvre tous les moyens raisonnables à sa disposition pour assurer un accès de qualité au site.
+
+            </p>
+        </div>
+
+        <hr>
+
+        <div>
+            <h4 class="bold font-weight-600 mb-3">Propriété intellectuelle</h4>
+            <p class="font-weight-500">
+                La structure générale du site www.tendaxe.com , ainsi que les textes, graphiques, images, sons et vidéos la composant, sont la propriété de l'éditeur ou de ses partenaires.
+                <br>
+                Toute représentation et/ou reproduction et/ou exploitation partielle ou totale des contenus et services proposés par le site www.tendaxe.com , par quelque procédé que ce soit, sans l'autorisation préalable et par écrit de TendAxe® est strictement interdite et serait susceptible de constituer une contrefaçon.
+
+            </p>
+        </div>
+
+        <hr>
+
+        <div>
+            <h4 class="bold font-weight-600 mb-3">Sanctions</h4>
+            <p class="font-weight-500">
+                TendAxe® se réserve le droit de refuser/supprimer l'accès au site www.tendaxe.com unilatéralement et avec notification préalable, à tout utilisateur ne respectant pas les présentes conditions d'utilisation.
+            </p>
+        </div>
+
+        <hr>
+
+        <div>
+            <h4 class="bold font-weight-600 mt-3">Evolution des présentes conditions d’utilisation</h4>
+            <p class="font-weight-500">
+                www.tendaxe.com se réserve le droit de modifier les termes, conditions et mentions des présentes conditions d’utilisation à tout moment. 
+                <br>
+                Il est ainsi conseillé à l'utilisateur de consulter régulièrement la dernière version des conditions d'Utilisation.
+            </p>
+        </div>
+
+        <hr>
+
+        <div>
+            <h4 class="bold font-weight-600 mb-3">Publication d’offres et sous-tritances</h4>
+            <p class="font-weight-500">
+                Dans le cas où vous publiez vos appels d'offres, demande d'achats ou des sous-traitance il faut avoir la crédibilité précisant les différentes informations de vos projets et de votre entreprise
+            </p>
+        </div>
+
+        <hr>
+
+        <div>
+            <h4 class="bold font-weight-600 mb-3">Renseignements et réclamations</h4>
+            <p class="font-weight-500">
+                Pour toute éventuelle demande d’information ou contestation relativement aux conditions
+                Générales d’utilisation et /ou fonctionnement du site l’utilisateur est convié à contacter notre service de client.
+            </p>
+        </div>
+    </div>
+@endsection
